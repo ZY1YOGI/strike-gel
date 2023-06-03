2d18805be13a9f70462d59f9b5267adad3dc1d17
ZY1YOGI <zy1yogi@gmail.com>
2023-06-04 00:14:01 +0300

    App V0.8

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css'
-import App from './App'
+import App from '@/App'
+import Thanks from '@/thanks'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/thanks' element={<Thanks />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 )
---
+++ src/thanks.tsx
@@ -0,0 +1,13 @@
+export default function Thanks() {
+  return (
+    <main className="main">
+      <div className="thanks-container">
+        <h1>THANK YOU!</h1>
+        <img src="./img/ch2.png" alt="" />
+        <h3>تم تسجيل طلبك للمنتج بنجاح</h3>
+        <h3>سوف يقوم أحد ممثلي خدمة العملاء بالتواصل معكم</h3>
+        <div className="re"><a id="go-back" href="./">الرجوع للموقع</a></div>
+      </div>
+    </main>
+  );
+}
