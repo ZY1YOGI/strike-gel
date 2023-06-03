4f8a646426c3482807fcdc996a9ab64fff5824b7
ZY1YOGI <zy1yogi@gmail.com>
2023-06-03 14:55:20 +0300

    section hero done

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,69 +4,68 @@ import imgArrow from "@/assets/images/arrow.png";
 export default function Hero() {
   return (
     <section id="hero" className="hero">
-      <div className="content">
-        <div className="img-and-product">
-          <img className="product" src={imgProduct} alt="" />
+      <div className="hero-content">
+        <div className="hero-img-product">
+          <img src={imgProduct} alt="" />
         </div>
 
-        <div className="top-sec">
-          <h2>الحل السحــري</h2>
-          <h2 className="border-btm">للضعف عند الرجال</h2>
+        <div className="hero-header">
+          <h2 className="hero-header-title">الحل السحــري</h2>
+          <h2 className="hero-header-subtitle">للضعف عند الرجال</h2>
           <h4>
             المنتــج الأكثــر <br /> مبيـعـا في الـعالــم
           </h4>
         </div>
 
-        <img className="arrow-img" src={imgArrow} alt="" />
+        <img className="hero-arrow-img" src={imgArrow} alt="" />
 
-        <div className="bottom-sec">
+        <div className="hero-footer">
           <div className="all-line">
             <div className="line">
               <span></span>
-              <div className="row">
+              <ul>
                 <li>يعطيــــــك انــتصــــــــاب قــــــوي وصلـــــــب</li>
                 <li>و يقضــــــي نهــائيـــــــــا علـــي الـضـعــــــــف</li>
-              </div>
+              </ul>
             </div>
 
             <div className="line">
               <span></span>
-              <div className="row">
+              <ul>
                 <li>يعمـــل علي اطـــالـــة العضــو الذكــري طولا</li>
                 <li>وعرضــا حتــي تصــل الـي درجـة الفحــــولــة</li>
-              </div>
+              </ul>
             </div>
 
             <div className="line">
               <span></span>
-              <div className="row">
+              <ul>
                 <li>يقــضي تمــامـــــــا علـــي ســرعـــة الـقــــذف</li>
                 <li>لعــــــلاقة اطـــول وامتـــع بيـــن الطـــــرفين</li>
-              </div>
+              </ul>
             </div>
 
             <div className="line">
               <span></span>
-              <div className="row">
+              <ul>
                 <li>يـــزيــــد الرغبـــة الجنسيــــة ويـــرفـــع مــــن</li>
                 <li>الاداء والـــقــــــوة للــــرجــــال لــتســـتعـيــــد</li>
-                <li>
-                  قــــوتـــــك وثـقـتـــــك فـــــي الـعـــلاقــــــــــة
+                <li>قــــوتـــــك وثـقـتـــــك فـــــي الـعـــلاقــــــــــة
                 </li>
-              </div>
+              </ul>
             </div>
 
             <div className="line">
               <span></span>
-              <div className="row">
-                <li>امــــن وصـحــــي لأنـــــه مــــن اعــشـــــــــاب</li>
+              <ul>
+                <li>أمــــن وصـحــــي لأنـــــه مــــن اعــشـــــــــاب</li>
                 <li>وفـيتــامينـــــات طــــبيعيــــــة </li>
-              </div>
+              </ul>
             </div>
           </div>
-
+        </div>
+        <div className="text-center">
           <a href="#form" className="btn">
-            {" "}
             اطلب الان
           </a>
         </div>
