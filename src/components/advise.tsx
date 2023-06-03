e53d3007013222942c9facbabcabcbc2747ca8b7
ZY1YOGI <zy1yogi@gmail.com>
2023-06-03 17:48:32 +0300

    section results v2.5 & advise & how

--- src/components/advise.tsx
+++ src/components/advise.tsx
@@ -2,60 +2,58 @@ import imgDoctor from "@/assets/images/doctor.png";
 
 export default function Advise() {
   return (
-    <section className="section-3">
-      <div className="contanier">
-        <div className="div-text">
-          <div className="top-sec"></div>
-          <div className="bottom-sec">
-            <div className="head">
-              <p>ينصح الدكتور البروفيسور</p>
-              <span>نيكـــولاوس ميـرتزيــوتـس</span>
-              <p>بــأستخدام سترايـك جيل</p>
+    <section className="advise">
+      <div className="advise-container">
+
+        <div className="advise-header">
+
+          <div className="head">
+            <p>ينصح الدكتور البروفيسور</p>
+            <span>نيكـــولاوس ميـرتزيــوتـس</span>
+            <p>بــأستخدام سترايـك جيل</p>
+          </div>
+
+          <div className="all-line">
+
+            <div className="line">
+              <span></span>
+              <ul>
+                <li>لأنـه يعطيــك نتيـجة فوريـة</li>
+                <li>سـريـعة منـذ اول استـخدام</li>
+              </ul>
             </div>
 
-            <div className="all-line">
-              <div className="line">
-                <span></span>
-                <div className="row">
-                  <li>لأنـه يعطيــك نتيـجة فوريـة</li>
-                  <li>سـريـعة منـذ اول استـخدام</li>
-                </div>
-              </div>
+            <div className="line">
+              <span></span>
+              <ul>
+                <li>لأنـــه كـورس علاجي نهائي</li>
+                <li>ولــيـــــس حـــــــل مـــــؤقـت</li>
+              </ul>
+            </div>
 
-              <div className="line">
-                <span></span>
-                <div className="row">
-                  <li>لأنـــه كـورس علاجي نهائي</li>
-                  <li>ولــيـــــس حـــــــل مـــــؤقـت</li>
-                </div>
-              </div>
+            <div className="line">
+              <span></span>
+              <ul>
+                <li>امــــن وصحـي علي مرضـي </li>
+                <li>القلـب والضغـــط والـسكــر</li>
+              </ul>
+            </div>
 
-              <div className="line">
-                <span></span>
-                <div className="row">
-                  <li>امــــن وصحـي علي مرضـي </li>
-                  <li>القلـب والضغـــط والـسكــر</li>
-                </div>
-              </div>
-
-              <div className="line">
-                <span></span>
-                <div className="row">
-                  <li>لأنه سـيجعلك بقـوة شـاب</li>
-                  <li>فــــي الــــــ20 مـــن عــمـــــره </li>
-                </div>
-              </div>
+            <div className="line">
+              <span></span>
+              <ul>
+                <li>لأنه سـيجعلك بقـوة شـاب</li>
+                <li>فــــي الــــــ 20 مـــن عــمـــــره </li>
+              </ul>
             </div>
           </div>
         </div>
-        <div className="div-img">
-          <img src={imgDoctor} alt="" />
-        </div>
+
+        <img src={imgDoctor} alt="" />
+
       </div>
 
-      <a href="#form" className="btn">
-        اطلب الان
-      </a>
+      <a href="#form" className="btn">اطلب الان</a>
     </section>
   );
 }
--- src/components/how.tsx
+++ src/components/how.tsx
@@ -2,22 +2,20 @@ import img6 from "@/assets/images/6.png";
 
 export default function How() {
   return (
-    <section className="section-4 two-sec">
-      <div className="content">
-        <div className="top-sec">
-          <h2>كيف يعمل سترايك جيل</h2>
-          <h2>علي تقوية الانتصاب</h2>
-        </div>
+    <section className="how">
+      <div className="how-header">
+        <h2>كيف يعمل سترايك جيل</h2>
+        <h2>علي تقوية الانتصاب</h2>
+      </div>
 
-        <div className="botm-sec">
-          <img src={img6} alt="" />
-          <div className="text">
-            <p>تتوغـل مكونات اسـترايك جيل داخل البطــانـــة</p>
-            <p>الحساســـة للعضــو الذكــري مما يساعــد علـي</p>
-            <p>زيــادة هـرمــون التستوستيرون وتنشيـــط الخلاية</p>
-            <p>الخاملـة واعطاء القوة والصلابة وأطالة حجـــم</p>
-            <p>العضــــو طــولا وعرضـآ واعطـــاء انتصــاب قــوي</p>
-          </div>
+      <div className="how-footer">
+        <img src={img6} alt="" />
+        <div className="text">
+          <p>تتوغـل مكونات اسـترايك جيل داخل البطــانـــة</p>
+          <p>الحساســـة للعضــو الذكــري مما يساعــد علـي</p>
+          <p>زيــادة هـرمــون التستوستيرون وتنشيـــط الخلاية</p>
+          <p>الخاملـة واعطاء القوة والصلابة وأطالة حجـــم</p>
+          <p>العضــــو طــولا وعرضـآ واعطـــاء انتصــاب قــوي</p>
         </div>
       </div>
     </section>
