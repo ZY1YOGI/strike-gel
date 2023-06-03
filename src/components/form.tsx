bc1112b2b2d5aaa94344e51d959d3437127a4105
ZY1YOGI <zy1yogi@gmail.com>
2023-06-04 01:51:53 +0300

    App V0.9.9

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -33,14 +33,14 @@ export default function Form() {
     <form onSubmit={onSubmitForm} action="https://api.sheetmonkey.io/form/nv3q7KdX21CFQnrb6c9GCT" method="post" id="form" className="form">
 
       <input type="hidden" name="Created" value="x-sheetmonkey-current-date-time" />
-      <input  type="hidden"  name="x-sheetmonkey-redirect"  value={`${window.location.hostname}/thanks`} />
+      <input  type="hidden"  name="x-sheetmonkey-redirect"  value={`${document.URL}/thanks`} />
       <div className="form-control">
         <input type="text" name="name" id="name" placeholder="الاسم" value={name} onChange={e => setName(e.target.value)} />
         <small>{errorName}</small>
       </div>
 
       <div className="form-control">
-        <input type="text" name="phone" id="phone" placeholder="رقم الهاتف" value={phone} onChange={e => setPhone(e.target.value)} />
+        <input type="number" name="phone" id="phone" placeholder="رقم الهاتف" value={phone} onChange={e => setPhone(e.target.value)} />
         <small>{errorPhone}</small>
       </div>
 
